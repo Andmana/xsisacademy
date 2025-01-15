93379d9cbe95e93232577630472728b944f2d223
andmana <iam.andmana@gmail.com>
2025-01-15 09:59:15 +0700

    Fix small mistakes

--- src/main/java/com/pembekalan/xsisacademy/entity/User.java
+++ src/main/java/com/pembekalan/xsisacademy/entity/User.java
@@ -1,6 +1,5 @@
 package com.pembekalan.xsisacademy.entity;
 
-import groovy.transform.EqualsAndHashCode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
--- src/main/java/com/pembekalan/xsisacademy/repository/UserRepository.java
+++ src/main/java/com/pembekalan/xsisacademy/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.pembekalan.xsisacademy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pembekalan.xsisacademy.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
     
 }
